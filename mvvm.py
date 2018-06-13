1107741a8933787f07f1493cf22af0281760f940
Mark24code <mark.zhangyoung@qq.com>
2018-06-13 10:04:41 +0800

    update

--- mvvm.py
+++ mvvm.py
@@ -3,57 +3,73 @@ import json
 
 class MVVMCore:
     def __init__(self):
-        self.dataStore = {}
-        self.subscribersHub = {}
+        self.data_store = {}
+        self.subs_hub = {}
 
-    def setData(self, key, value):
-        self.dataStore[key] = value
+    def set_data(self, key, value):
+        self.data_store[key] = value
         self.emmit(key)
 
     def emmit(self, key):
-        if key in self.subscribersHub:
-            for sub in self.subscribersHub[key]:
-                sub.update(key, self.dataStore[key])
+        if key in self.subs_hub:
+            for sub in self.subs_hub[key]:
+                sub.update(key, self.data_store[key])
 
     def subscribe(self, key, obj):
-        if key in self.subscribersHub:
-            self.subscribersHub[key].append(obj)
+        if key in self.subs_hub:
+            self.subs_hub[key].append(obj)
         else:
-            self.subscribersHub[key] = [obj]
+            self.subs_hub[key] = [obj]
 
-    def toJson(self):
-        print('store:{}'.format(json.dumps(self.dataStore)))
-        print('subscribersHub:{}'.format(json.dumps(self.subscribersHub)))
+    def unsubscribe(self, key, obj):
+        if key in self.subs_hub:
+            self.subs_hub[key].remove(obj)
+
+    def to_json(self):
+        print('store:{}'.format(json.dumps(self.data_store)))
+        print('subs_hub:{}'.format(json.dumps(self.subs_hub)))
 
 
-class Sub:
-    def __init__(self, name):
+class Temp:
+    def __init__(self, name, mount):
         self.name = name
-        print('初始化Sub：{}'.format(name))
+        self.mount = mount
+        print('初始化Temp：{}'.format(name))
 
-    def subscribe(self, system, key):
+    def subscribe(self, key):
         print('[{}]: Subscribe => {}'.format(self.name, key))
-        system.subscribe(key, self)
+        self.mount.subscribe(key, self)
+
+    def unsubscribe(self, key):
+        print('[{}]: Unsubscribe => {}'.format(self.name, key))
+        self.mount.unsubscribe(key, self)
 
     def update(self, key, value):
-        print('[{}]: Render Update => {}:{}'.format(self.name, key, value))
+        print('[{}]: Template Update => {}:{}'.format(self.name, key, value))
 
 
 wxcore = MVVMCore()
 print('---初始化----')
-print(wxcore.toJson())
+print(wxcore.to_json())
 print('--设置数据---')
-wxcore.setData('word', 'hello word')
-wxcore.setData('age', 12)
-wxcore.setData('height', 180)
-print(wxcore.toJson())
+wxcore.set_data('word', 'hello word')
+wxcore.set_data('age', 12)
+wxcore.set_data('height', 180)
+print(wxcore.to_json())
 print('--订阅者实例---')
-templte1 = Sub('template1')
-templte2 = Sub('template2')
-templte3 = Sub('template3')
+templte1 = Temp('template1', wxcore)
+templte2 = Temp('template2', wxcore)
+templte3 = Temp('template3', wxcore)
 print('---订阅-----')
-templte1.subscribe(wxcore, 'word')
-templte2.subscribe(wxcore, 'age')
-templte3.subscribe(wxcore, 'height')
-print('---触发dataChange---')
-wxcore.setData('height', 900)
+templte1.subscribe('word')
+templte1.subscribe('age')
+templte2.subscribe('age')
+templte3.subscribe('height')
+print('---触发dataChange Height---')
+wxcore.set_data('height', 900)
+print('---触发dataChange Age---')
+wxcore.set_data('age', 900)
+print('---templte1 解绑---')
+templte1.unsubscribe('age')
+print('---触发dataChange Age---')
+wxcore.set_data('age', 100)
