5b8717ab27db48da5e83dca78a72b6db80b0b7fc
Mark24code <mark.zhangyoung@qq.com>
2019-09-12 15:28:10 +0800

    refactor(更加简化,减少一层嵌套):

--- 2019-09-12 断线重连.js
+++ 2019-09-12 断线重连.js
@@ -1,6 +1,6 @@
 // 请求
 async function request(arg) {
-  return 10000
+  return null
 }
 
 // 延时
@@ -11,32 +11,25 @@ const waiting = function (waitTime) {
 }
 
 // 尝试重连
-async function tryConnect(arg) {
-  const MAX_TRY = 3
-  let count = 1;
+async function tryConnect(arg, leftChance = 3) {
 
   // 重连
-  async function letTry(arg) {
-    let anwser;
-    if (count > MAX_TRY) {
-      return '暂无回答'
-    }
-    try {
-      anwser = await request(arg);
-      console.log(`尝试第${count}次,参数:${arg},结果:${anwser}`)
-      if (!anwser) {
-        //进入尝试
-        await waiting(1000);
-        count += 1;
-        return await letTry(arg);
-      }
-      return anwser;
-    } catch (err) {
-      return '报错'
-    }
+  let anwser;
+  if (leftChance <= 0) {
+    return '暂无回答'
   }
-
-  return await letTry(arg);
+  try {
+    anwser = await request(arg);
+    console.log(`尝试第${leftChance}次,参数:${arg},结果:${anwser}`)
+    if (!anwser) {
+      //进入尝试
+      await waiting(1000);
+      return tryConnect(arg, leftChance - 1);
+    }
+    return anwser;
+  } catch (err) {
+    return '报错'
+  }
 }
 
 
